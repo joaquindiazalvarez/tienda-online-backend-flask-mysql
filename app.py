554eeac6b5de35a601296ca814bcc4a2b9a0bdbe
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-07-16 20:38:28 -0400

    heroku config

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ from config import config
 
 app = Flask(__name__)
 
+# Config MySQL
+app.config['MYSQL_HOST'] = 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'bsale_test'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_PASSWORD'] = 'bsale_test'
+app.config['MYSQL_DB'] = 'bsale_test'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 conexion = MySQL(app)
 
