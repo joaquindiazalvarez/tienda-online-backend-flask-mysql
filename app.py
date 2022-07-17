c82d4cfd72594e9d2d49c689f28d78b5bf466612
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-07-16 20:47:41 -0400

    heroku again

--- app.py
+++ app.py
@@ -19,15 +19,13 @@ conexion = MySQL(app)
 
 @app.route('/')
 def list_products():
-    try:
-        cursor=conexion.connection.cursor()
-        sql = "SELECT id, name, url_image, price, discount, category FROM product"
-        cursor.execute(sql)
-        datos=cursor.fetchall()
-        print(datos)
-        return "products listed"
-    except Exception as ex:
-        return json.dumps(ex)
+    cursor=conexion.connection.cursor()
+    sql = "SELECT id, name, url_image, price, discount, category FROM product"
+    cursor.execute(sql)
+    datos=cursor.fetchall()
+    print(datos)
+    return "products listed"
+    
 @app.route('/categories/getbycategory', methods = ['POST'])
 def get_products_by_category():
     body = request.get_json()
@@ -75,4 +73,4 @@ def page_not_found(error):
 if __name__ == '__main__':
     app.config.from_object(config['development'])    
     app.register_error_handler(404, page_not_found) 
-    app.run()
+    app.run(debug=True)
