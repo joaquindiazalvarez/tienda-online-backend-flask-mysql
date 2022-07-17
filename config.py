864ca7dbdbf3080d8fb1e289e03a323891a2051f
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-07-17 00:46:51 -0400

    deleted connection in gonfig file

--- config.py
+++ config.py
@@ -1,16 +1,5 @@
 class DevelopmentConfig():
-    """
-    configuramos flask para que
-    reconozca la base de datos
-    de BSALE
-    """
     DEBUG = True
-    MYSQL_HOST = 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com'
-    MYSQL_USER = 'bsale_test'
-    MYSQL_PASSWORD = 'bsale_test'
-    MYSQL_DB = 'bsale_test'
-
-
 config = {
         'development':DevelopmentConfig
         }
