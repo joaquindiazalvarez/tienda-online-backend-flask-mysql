0d35bbb2c7df30f1119652bc3d6a7db9bdc65291
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-07-19 11:38:19 -0400

    comentarios

--- app.py
+++ app.py
@@ -4,33 +4,60 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 load_dotenv()
+#se cargen las variables de enviroment
+
+"""
+###INSTRUCCIONES###
+cursor=conexion.connection.cursor()
+#permite al código de python ejecutar un comando en la sesión de la db
+
+cursor.execute(sql)
+#ejecuta la query en la db
+
+data=cursor.fetchall()
+#guarda el resultado de la query a la db en una variable
+###
+"""
 
 
 app = Flask(__name__)
+#se crea una instancia del objeto Flask(se crea la aplicación)
 cors = CORS(app)
-
-
+#esta instancia se encarga del CORS
 
 HOST = os.getenv("HOST")
 USER = os.getenv("USER")
 PASSWORD = os.getenv("PASSWORD")
 DB = os.getenv("DB")
-# Config MySQL
+#se setean las variables de enviroment
+
 app.config['MYSQL_HOST'] = HOST
 app.config['MYSQL_USER'] = USER
 app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_PASSWORD'] = PASSWORD
 app.config['MYSQL_DB'] = DB
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#se configura la conexión a la base de datos
 
 conexion = MySQL(app)
+#se establece la conexión de Flask con la base de datos
 
 @app.route('/')
 def say_hello():
+    """
+    Endpoint de ejemplo
+    args: ninguno
+    retorna: un saludo
+    """
     return '<h1>Hello World, this is my online-store API</h1>'
 
 @app.route('/getall', methods = ['GET'])
 def list_products():
+    """
+    Endpoint para traer todos los productos
+    args: ninguno
+    return: un JSON con todos los productos
+    """
     cursor=conexion.connection.cursor()
     sql = "SELECT id, name, url_image, price, discount, category FROM product"
     cursor.execute(sql)
@@ -40,8 +67,20 @@ def list_products():
     
 @app.route('/categories/getbycategory/<int:page>/', methods = ['POST'])
 def get_products_by_category(page):
+    """
+    Endpoint para traer productos que tengan la categoría señalada
+    args:
+        ruta:
+            page, indica la página que desea traerse
+        body:
+            category, se traen solo los productos con la categoría indicada
+            discount, si es True, se traen solo los productos con descuento
+            order, se ordenan los productos según order
+    return: JSON con productos con la categoría señalada, ordenados y paginados
+    """
     body = request.get_json()
     page_limit = 12
+    #se setea cuantos productos irán por página
     cursor = conexion.connection.cursor()
     if "discount" in body:
         if body['discount'] == True:
@@ -50,6 +89,7 @@ def get_products_by_category(page):
         else: 
             discount = ""
             discount_where = ""
+    #sentencias que se agregan a la query para filtrar por descuento
     else:
         dic = {'message': "must specify discount"}
         return jsonify(dic)
@@ -66,19 +106,25 @@ def get_products_by_category(page):
         elif body['order'] == "pricemay":
             parameter = "price"
             order = "DESC"
+        #se setean el template para ordenar la query
         if body['category'] == "todos":
-            sql = sql = f"SELECT id, name, url_image, price, discount, category FROM product {discount_where} ORDER BY {parameter} {order}"
+            #en este caso la categoría es todos. Se traen todos los productos que cumplan con el orden, la paginación y el descuento
+            sql = f"SELECT id, name, url_image, price, discount, category FROM product {discount_where} ORDER BY {parameter} {order}"
             cursor.execute(sql)
             data = cursor.fetchall()
             first_element = (page - 1)* page_limit
             last_element = (page * page_limit)
+            #se calcula cual debe ser el primer y ultimo elemento de la página
             data_cut = data[first_element:last_element]
+            #se toma la parte del arreglo que corresponde a la paginación
             dic = {'message': "Success", 'products': data_cut, 'pages': len(data)//page_limit + 1, 'products_length':len(data)}
             if page < 1 or page > dic['pages']:
                 dic = {'message': "Page out of range"}
                 return jsonify(dic)
             return jsonify(dic)
         sql = f"SELECT id FROM category WHERE name = '{body['category']}'"
+        #si no se cumple que la categoría sea todos, se ejecuta otra query
+        #que convierte el string de categoría en un id
         cursor.execute(sql)
         id_dic = cursor.fetchone()
         if id_dic:
@@ -88,7 +134,9 @@ def get_products_by_category(page):
             data = cursor.fetchall()
             first_element = (page - 1)* page_limit
             last_element = (page * page_limit)
+            #se calcula cual debe ser el primer y último elemento de la página
             data_cut = data[first_element:last_element]
+            #se toma la parte del arreglo que corresponde a la paginación
             dic = {'message': "Success", 'products': data_cut, 'pages':len(data)//page_limit + 1, 'products_length':len(data)}
             if page < 1 or page > dic['pages']:
                 dic = {'message': "Page out of range"}
@@ -104,7 +152,19 @@ def get_products_by_category(page):
 
 @app.route('/search/<int:page>/', methods = ['POST']) 
 def search(page):
+    """
+    Endpoint para traer productos que tengan la categoría señalada
+    args:
+        ruta:
+            page, indica la página que desea traerse
+        body:
+            search, se traen los productos que contengan este string en el nombre
+            discount, si es True, se traen solo los productos con descuento
+            order, se ordenan los productos según order
+    return: JSON con productos con la búsqueda señalada, ordenados y paginados
+    """
     page_limit = 12
+    #se setea cuantos productos irán por página
     map = {"bebida energetica": 1, "pisco": 2, "ron": 3, "bebida": 4, "snack":5, "cerveza":6, "vodka":7, "todos": "category"}
     body = request.get_json()  
     category = map[body['category']]
@@ -113,6 +173,7 @@ def search(page):
             discount = "AND discount > '0'"
         else: 
             discount = ""
+    #sentencias que se agregan a la query para filtrar por descuento
     else:
         dic = {'message': "must specify discount"}
         return jsonify(dic)
@@ -131,6 +192,7 @@ def search(page):
             elif body['order'] == "pricemay":
                 parameter = "price"
                 order = "DESC"
+            #se setean el template para ordenar la query
             sql = f"SELECT id, name, url_image, price, discount, category FROM product WHERE name LIKE '%{body['search']}%' AND category = {category} {discount} ORDER BY {parameter} {order}"
             cursor.execute(sql)
             data = cursor.fetchall()
@@ -138,7 +200,9 @@ def search(page):
                 return jsonify({'message': "0 Results", 'products':[]})
             first_element = (page - 1)* page_limit
             last_element = (page * page_limit)
+            #se calcula cual debe ser el primer y ultimo elemento de la página
             data_cut = data[first_element:last_element]
+            #se toma la parte del arreglo que corresponde a la paginación
             dic = {'messagge': "Success", 'products': data_cut, 'pages':len(data)//page_limit + 1, 'products_length':len(data)}
             if page < 1 or page > dic['pages']:
                 dic = {'message': "Page out of range"}
@@ -151,8 +215,13 @@ def search(page):
     return jsonify(dic)
 
 def page_not_found(error):
+    """
+    función que capta las rutas que no están definidas
+    return: mensaje
+    """
     return "<h1>The page that you're looking for was not found ...</h1>"
 
 if __name__ == '__main__': 
     app.register_error_handler(404, page_not_found) 
     app.run()
+    #corre la aplicación
